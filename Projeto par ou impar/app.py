1ae6a0dea1e2b8304ca27a1c74b67132a683a2be
Lucas Amancio <74212108+lucasskt18@users.noreply.github.com>
2021-11-14 17:58:53 -0300

    Add files via upload

---
+++ Projeto par ou impar/app.py
@@ -0,0 +1,10 @@
+# projeto par ou impar 
+while True:
+  try:
+     valor = int(input('Digite um valor'))
+    if valor % 2 == 0:
+        print('Número par')
+        else:
+        print('Número impar')
+   except:
+      print('Digite apenas números'))
